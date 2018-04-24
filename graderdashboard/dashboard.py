ad6229a49ddbb8f6d0d6d4fa51fc7336cf17b3b1
zachglassman <zach.glassman@gmail.com>
2018-04-24 14:50:08 -0700

    working application with student and global views

--- graderdashboard/dashboard.py
+++ graderdashboard/dashboard.py
@@ -1,6 +1,25 @@
+from collections import defaultdict
+from itertools import chain
+from datetime import datetime
 from flask import Flask, request, render_template, request
+from bokeh.models import ColumnDataSource, FactorRange
+from bokeh.plotting import figure
+from bokeh.embed import components
+from bokeh.layouts import column
 from .models import db
 from .gradeservice import GradeService
+
+def _flatten(x):
+    return tuple(chain.from_iterable(x))
+
+PROJECTS = {
+    'dw':['summary_statistics', 'most_common_item', 'items_by_region', 'script_anomalies', 'script_growth', 'rare_scripts'],
+    'in':['problem1', 'problem2'],
+    'ip':['mersenne_numbers', 'lucas_lehmer', 'mersenne_primes', 'is_prime_fast', 'erastosthenes'],
+    'pw':['summary_statistics', 'most_common_item', 'items_by_region'],
+    'vc':['point_repr', 'add_subtract', 'multiplication', 'distance', 'k_means'],
+    'ml':['billing_model', 'balanced_billing_model', 'demo_model', 'ensemble_model'] 
+}
 
 def create_app(db_uri, brand):
 
@@ -14,14 +33,53 @@ def create_app(db_uri, brand):
     def render_with_brand(route, **kwargs):
         return render_template(route, brand=brand, **kwargs)
 
+    def _plot_subs(overview):
+        days = list(overview.keys())
+        subs = [overview[i] for i in days]
+
+        p1 = figure(x_axis_type='datetime', plot_height=250,
+                    title='Submissiosn per Day')
+        plot_days = [datetime.strptime(i, "%Y-%m-%d") for i in days]
+        p1.line(plot_days, subs)
+        p1.circle(plot_days, subs)
+        return p1
+
     @app.route('/')
     def index():
         subs = gs.overview()
-        return render_with_brand('index.html', subs=subs)
+        overview = gs.day_overview()
+        script, div = components(_plot_subs(overview))
+        return render_with_brand('index.html', 
+                                 subs=subs,
+                                 div=div,
+                                 script=script)
 
     @app.route('/student/<student>')
     def student_overview(student):
-        info = gs.student_overview(student)
-        return render_with_brand('student_overview', info=info)
+        per_project = gs.student_per_project(student)
+        overview = gs.student_overview(student)
+        total_submissions = sum(overview.values())
+        p1 = _plot_subs(overview)
+
+        projects = defaultdict(dict)
+        for i in per_project:
+            project = i['problem'].split('__')[0]
+            projects[project][i['problem']] = i['score']
+
+        p_names = sorted(PROJECTS.keys())
+        x = _flatten([[(k, str(i)) for i, j in enumerate(PROJECTS[k])] for k in p_names])
+        scores = _flatten([[projects[k].get("{}__{}".format(k, j), 0) for j in PROJECTS[k]] for k in p_names])
+        source = ColumnDataSource(data=dict(x=x, 
+                                            scores=scores))
+        p = figure(x_range=FactorRange(*x), plot_height=250)
+        p.vbar(x='x', top='scores', source=source, width=0.9)
+        
+        script, div = components(column(p1, p))
+        return render_with_brand('student_overview.html', 
+                                 info=per_project, 
+                                 script=script, 
+                                 div=div,
+                                 name=student,
+                                 total=total_submissions)
 
     return app
--- graderdashboard/gradeservice.py
+++ graderdashboard/gradeservice.py
@@ -1,3 +1,4 @@
+from collections import defaultdict
 from .models import Grade
 
 class GradeService():
@@ -5,6 +6,14 @@ class GradeService():
     def __init__(self, db):
         self.db = db
         self._s = self.db.session
+
+    def day_overview(self):
+        s = self._s.query(Grade.question, Grade.submission_time)
+        d = defaultdict(int)
+        for i in s:
+            day = i[1].strftime("%Y-%m-%d")
+            d[day] += 1
+        return d
 
     def overview(self):
         submissions =  self._s.query(Grade.name, 
@@ -15,16 +24,25 @@ class GradeService():
 
         return [{'name': i[0], 'subs': i[1]} for i in submissions]
 
-    def student_per_project(self):
-        pass
+    def student_overview(self, student):
+        s = self._s.query(Grade.question, Grade.submission_time) \
+                          .filter(Grade.name == student)
+        d = defaultdict(int)
+        for i in s:
+            day = i[1].strftime("%Y-%m-%d")
+            d[day] += 1
+        return d
 
     def project_overview(self):
         pass
 
-    def student_overview(self, student):
-        s = self._s.query(Grade.question, self.db.func.max(Grade.score)) \
+    def student_per_project(self, student):
+        s = self._s.query(Grade.question, 
+                          self.db.func.max(Grade.score), 
+                          self.db.func.count(Grade.submission_time)) \
                           .filter(Grade.name == student) \
-                         .group_by(Grade.question).all()
-        return [{'problem': i[0], 'score': i[1]} for i in s]
+                          .group_by(Grade.question).all()
+        
+        return [{'problem': i[0], 'score': i[1], 'count' :i[2]} for i in s]
 
 
